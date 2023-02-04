5a176027da039ef0fe9755310275f95c0e59370c
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-02-04 15:23:44 +0100

    add docker files

--- first-app/api/config/db.js
+++ first-app/api/config/db.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     mongoose.set('strictQuery', false)
-    mongoose.connect(process.env.MONGODB_URI).then(() => console.log('mongoDB connected ...')).catch(err => {
+    mongoose.connect(process.env.MONGODB_URI, {
+        dbName: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('mongoDB connected ...')).catch(err => {
         console.log('Mongo DB connection error ...')
         console.log(err.messsage)
     })
